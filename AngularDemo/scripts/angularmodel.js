dab6c74c1c50cd057dcd144668669be0f8d45491
Akshay <Akshay@AKSHAY>
2017-07-20 17:45:45 +0530

    commit : new project

---
+++ AngularDemo/scripts/angularmodel.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular
+            .module("myModule", [])
+            .controller("myController", function ($scope) {
+
+                var employee = [
+                    { name: "kakashi", gender: "Male", age: 32 },
+                {name : "naruto" , gender : "Male", age : 21 },
+                {name : "sakura" , gender : "Female", age : 20 }
+
+                ]
+                $scope.employee = employee;
+
+            });
